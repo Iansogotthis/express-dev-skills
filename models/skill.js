1e1a818b263f902fb099ed9d966546eea3063a8a
= <=>
2024-02-13 20:52:07 -0600

    complete index functionality

---
+++ models/skill.js
@@ -0,0 +1,13 @@
+let skills = [
+    { id: 1, name: 'JavaScript', proficiency: 'Expert' },
+    { id: 2, name: 'HTML', proficiency: 'Intermediate' },
+    { id: 3, name: 'CSS', proficiency: 'Beginner' },
+  ]
+  module.exports = {
+    getAll
+  };
+  
+  function getAll() {
+    return skills;
+  }
+  
---
+++ routes/skills.js
@@ -0,0 +1,12 @@
+
+ const express = require('express');
+ const router = express.Router();
+
+
+ const skillsCtrl = require('../controllers/skills');
+
+
+ 
+ router.get('/', skillsCtrl.index);
+
+module.exports = router;
